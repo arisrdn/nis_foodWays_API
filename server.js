9732b1f5c2ed63ff15651ed017a200ee24fad335
Aris Ramadhan <arisram22@gmail.com>
2021-05-04 17:13:36 +0700

    ok

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-// require("dotenv").config();
+require("dotenv").config();
 const app = express();
 const cors = require("cors");
 
-// const router = require("./src/routes");
+const router = require("./src/routes");
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
-// app.use("/api/v1/", router);
+app.use("/api/v1/", router);
 app.use("/uploads", express.static("uploads"));
 //create the home route
 app.get("/", (req, res) => {
