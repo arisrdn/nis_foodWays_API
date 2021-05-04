e35d16511cd874f31cdb7ad541d22da59e05ceec
Aris Ramadhan <arisram22@gmail.com>
2021-05-04 17:27:26 +0700

    ok

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-require("dotenv").config();
+// require("dotenv").config();
 const app = express();
 const cors = require("cors");
-
 const router = require("./src/routes");
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/", router);
